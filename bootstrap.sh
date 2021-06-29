ef4eb17b2bbd61db62eb8a779d9665cfc404a298
Donald Tang <846161818@qq.com>
2021-06-29 23:19:02 +0800

    fix some bugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,11 +17,11 @@
 ############################  SETUP PARAMETERS
 
 app_name='vim-config'
-[[ -z "$APP_PATH" ]] && APP_PATH="$HOME/.$app_name"
-[[ -z "$REPO_URI" ]] && REPO_URI="https://github.com/Donaldttt/$app_name"
-[[ -z "$REPO_BRANCH" ]] && REPO_BRANCH='3.0'
+[ -z "$APP_PATH" ] && APP_PATH="$HOME/.$app_name"
+[ -z "$REPO_URI" ] && REPO_URI="https://github.com/Donaldttt/$app_name"
+[ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
-[[ -z "$VUNDLE_URI" ]] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
+[ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
 
 ############################  BASIC SETUP TOOLS
 msg() {
@@ -29,7 +29,7 @@ msg() {
 }
 
 success() {
-    if [[ "$ret" -eq '0' ]]; then
+    if [ "$ret" -eq '0' ]; then
         msg "\33[32m[✔]\33[0m ${1}${2}"
     fi
 }
@@ -40,7 +40,7 @@ error() {
 }
 
 debug() {
-    if [[ "$debug_mode" -eq '1' ]] && [[ "$ret" -gt '1' ]]; then
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
         msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
     fi
 }
@@ -50,7 +50,7 @@ program_exists() {
     command -v $1 >/dev/null 2>&1 || { local ret='1'; }
 
     # fail on non-zero return value
-    if [[ "$ret" -ne 0 ]]; then
+    if [ "$ret" -ne 0 ]; then
         return 1
     fi
 
@@ -61,19 +61,19 @@ program_must_exist() {
     program_exists $1
 
     # throw error on non-zero return value
-    if [[ "$?" -ne 0 ]]; then
+    if [ "$?" -ne 0 ]; then
         error "You must have '$1' installed to continue."
     fi
 }
 
 variable_set() {
-    if [[ -z "$1" ]]; then
+    if [ -z "$1" ]; then
         error "You must have your HOME environmental variable set to continue."
     fi
 }
 
 lnif() {
-    if [[ -e "$1" ]]; then
+    if [ -e "$1" ]; then
         ln -sf "$1" "$2"
     fi
     ret="$?"
@@ -83,11 +83,11 @@ lnif() {
 ############################ SETUP FUNCTIONS
 
 do_backup() {
-    if [[ -e "$1" ]] || [[ -e "$2" ]] || [[ -e "$3" ]]; then
+    if [ -e "$1" ] || [ -e "$2" ] || [ -e "$3" ]; then
         msg "Attempting to back up your original vim configuration."
         today=`date +%Y%m%d_%s`
         for i in "$1" "$2" "$3"; do
-            [[ -e "$i" ]] && [[ ! -L "$i" ]] && mv -v "$i" "$i.$today";
+            [ -e "$i" ] && [ ! -L "$i" ] && mv -v "$i" "$i.$today";
         done
         ret="$?"
         success "Your original vim configuration has been backed up."
@@ -103,7 +103,7 @@ sync_repo() {
 
     msg "Trying to update $repo_name"
 
-    if [[ ! -e "$repo_path" ]]; then
+    if [ ! -e "$repo_path" ]; then
         mkdir -p "$repo_path"
         git clone -b "$repo_branch" "$repo_uri" "$repo_path"
         ret="$?"
@@ -151,7 +151,7 @@ set_up_nvim() {
   nvim_config="$HOME/.config/nvim/init.vim"
   if program_exists "nvim"; then
     msg "Found neovim."
-    if [[ -f $nvim_config ]]; then
+    if [ -f "$nvim_config" ]; then
       msg "Found init.vim. backing up it"
       mv $nvim_config "${nvim_config}.backup"
     fi
